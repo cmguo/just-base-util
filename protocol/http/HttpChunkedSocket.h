f22cc55dbdba870ccc4c757c2604440a53539fa3
isxxguo <isxxguo@pptv.com>
2012-08-09 09:29:30 +0000

    /base/branches/1.2.1 /util/protocol/http/HttpChunkedSocket.h 直接接收数据的实现，以前只实现了发送
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.1@4469 b3a1389e-2159-5c43-9038-463bf877d0d9

--- protocol/http/HttpChunkedSocket.h
+++ protocol/http/HttpChunkedSocket.h
@@ -6,6 +6,8 @@
 #include "util/buffers/BufferSize.h"
 #include "util/buffers/SubBuffers.h"
 
+#include <framework/network/AsioHandlerHelper.h>
+
 #include <boost/asio/streambuf.hpp>
 #include <boost/asio/socket_base.hpp>
 #include <boost/bind.hpp>
@@ -69,6 +71,72 @@ namespace util
             buf.commit(5);
         }
 
+        bool recv_crlf(
+            boost::asio::streambuf & buf, 
+            std::size_t & len)
+        {
+            using namespace boost::asio;
+
+            if (buf.size() == 0) {
+                // 如果buf为空，填充一个字节，表示开始接收
+                // 输入的 len 表示类型，head：0，tail：1
+                buf.sputc((char)len);
+                len = 2;
+                return false;
+            } else {
+                char const * hex_buf = buffer_cast<char const *>(buf.data()) + buf.size() - 1;
+                if (*hex_buf == '\n') {
+                    hex_buf = buffer_cast<char const *>(buf.data());
+                    if (hex_buf == 0) {
+                        len = 0;
+                        ++hex_buf;
+                        while (true) {
+                            char h = *hex_buf;
+                            if (h >= '0' && h <= '9')
+                                h -= '0';
+                            else if (h >= 'A' && h <= 'F')
+                                h -= ('A' - 10);
+                            else if (h >= 'a' && h <= 'f')
+                                h -= ('a' - 10);
+                            else
+                                break;
+                            len = (len << 4) | h;
+                            ++hex_buf;
+                        }
+                        if (len == 0) {
+                            // 类型2表示最后的trailer
+                            buf.reset();
+                            buf.sputc((char)2);
+                            len = 2;
+                            return false;
+                        }
+                    } else if (*hex_buf == 1) {
+                        buf.reset();
+                        len = 0;
+                    } else if (*hex_buf == 2) {
+                        len = buf.size();
+                        buf.reset();
+                        if (len == 3) {
+                            // 不是trailer，已经是最后的CRLF了
+                            len = std::size_t(-1);
+                        } else {
+                            // 是一个trailer，继续接收
+                            buf.sputc((char)2);
+                            len = 2;
+                            return false;
+                        }
+                    }
+                    return true;
+                } else if (*hex_buf == '\r') {
+                    len = 1;
+                    return false;
+                } else {
+                    len = 2;
+                    return false;
+                }
+            }
+        }
+
         template <
             typename Socket
         >
@@ -110,27 +178,25 @@ namespace util
             static eof_t eof() { return eof_t(); }
 
         public:
-            // 重写receive，async_receive，read_some，async_read_some
+            // 重写receive，async_receive，read_some，async_receive
             template <typename MutableBufferSequence>
             std::size_t receive(
                 const MutableBufferSequence & buffers)
             {
-                if (rcv_buf_.size() > 0) {
-                    return copy(buffers);
-                } else {
-                    return socket_.receive(buffers);
-                }
+                boost::system::error_code ec;
+                std::size_t s = recv(buffers, 0, ec);
+                boost::asio::detail::throw_error(ec);
+                return s;
             }
 
             template <typename MutableBufferSequence>
             std::size_t receive(const MutableBufferSequence& buffers, 
                 boost::asio::socket_base::message_flags flags)
             {
-                if (rcv_buf_.size() > 0) {
-                    return copy(buffers);
-                } else {
-                    return socket_.receive(buffers, flags);
-                }
+                boost::system::error_code ec;
+                std::size_t s = recv(buffers, flags, ec);
+                boost::asio::detail::throw_error(ec);
+                return s;
             }
 
             template <typename MutableBufferSequence>
@@ -139,26 +205,169 @@ namespace util
                 boost::asio::socket_base::message_flags flags, 
                 boost::system::error_code& ec)
             {
-                if (rcv_buf_.size() > 0) {
-                    ec = boost::system::error_code();
-                    return copy(buffers);
-                } else {
-                    return socket_.receive(buffers, flags, ec);
+                std::size_t bytes_recv = 0;
+                std::size_t bytes_left = buffers::buffers_size(buffers);
+                while (true) {
+                    if (rcv_buf_.size()) {
+                        // 剩余的Chunk头部或者尾部数据
+                        std::size_t bytes_transferred = socket_.receive(rcv_buf_.prepare(rcv_left_), flags, ec);
+                        rcv_buf_.consume(bytes_transferred);
+                        rcv_left_ -= bytes_transferred;
+                        if (rcv_left_) {
+                            break;
+                        }
+                        if (!recv_crlf(rcv_buf_, rcv_left_)) {
+                            continue;
+                        }
+                        if (rcv_left_ == std::size_t(-1)) {
+                            ec = boost::asio::error::eof;
+                            break;
+                        }
+                    }
+                    if (rcv_left_ == 0) {
+                        // 刚刚接收了尾部，重组新的Chunk
+                        if (bytes_left == 0)
+                            break;
+                        recv_crlf(rcv_buf_, rcv_left_);
+                    } else {
+                        // 接收完头部，接收剩余数据
+                        std::size_t bytes_transferred = socket_.receive(
+                            buffers::sub_buffers(buffers, bytes_recv, rcv_left_), flags, ec);
+                        bytes_recv += bytes_transferred;
+                        bytes_left -= bytes_transferred;
+                        rcv_left_ -= bytes_transferred;
+                        if (rcv_left_ == 0) {
+                            // 接收完数据，构建尾部
+                            recv_crlf(rcv_buf_, rcv_left_ = 1);
+                        }
+                        // 只要读到数据就退出
+                        break;
+                    }
                 }
+                return bytes_recv;
             }
+
+            template <typename MutableBufferSequence, typename ReadHandler>
+            struct recv_handler
+            {
+                typedef void result_type;
+
+                recv_handler(
+                    Source & source, 
+                    std::size_t & rcv_left, 
+                    boost::asio::streambuf & rcv_buf_, 
+                    MutableBufferSequence const & buffers, 
+                    ReadHandler handler)
+                    : socket_(source)
+                    , rcv_left_(rcv_left)
+                    , rcv_buf_(rcv_buf_)
+                    , buffers_(buffers)
+                    , handler_(handler)
+                    , bytes_recv_(0)
+                    , bytes_left_(buffers::buffers_size(buffers))
+                {
+                }
+
+                void start()
+                {
+                    if (rcv_buf_.size()) {
+                        socket_.async_receive(rcv_buf_.prepare(rcv_left_), flags_, 
+                            boost::bind(boost::ref(*this), _1, _2));
+                    } else {
+                        if (rcv_left_ == 0) { 
+                            if (bytes_left_ == 0) {
+                                socket_.get_io_service().post(
+                                    boost::bind(handler_, boost::system::error_code(), bytes_recv_));
+                                delete this;
+                                return;
+                            }
+                            rcv_left_ = bytes_left_;
+                            recv_crlf(rcv_buf_, rcv_left_);
+                            socket_.async_receive(buffers::sub_buffers(buffers_, bytes_recv_, bytes_left_), flags_, 
+                                boost::bind(boost::ref(*this), _1, _2));
+                        } else {
+                            socket_.async_receive(buffers::sub_buffers(buffers_, bytes_recv_, bytes_left_), flags_, 
+                                boost::bind(boost::ref(*this), _1, _2));
+                        }
+                    }
+                }
+
+                void operator()(
+                    boost::system::error_code const & ec, 
+                    size_t bytes_transferred)
+                {
+                    if (ec) {
+                        handler_(ec, bytes_recv_);
+                        delete this;
+                        return;
+                    }
+                    if (rcv_buf_.size()) {
+                        // 剩余的Chunk头部或者尾部数据
+                        rcv_buf_.consume(bytes_transferred);
+                        rcv_left_ -= bytes_transferred;
+                        if (rcv_left_) {
+                            handler_(ec, bytes_recv_);
+                            delete this;
+                            return;
+                        }
+                        if (!recv_crlf(rcv_buf_, rcv_left_)) {
+                            socket_.async_receive(rcv_buf_.prepare(rcv_left_), flags_, 
+                                boost::bind(boost::ref(*this), _1, _2));
+                            return;
+                        }
+                        if (rcv_left_ == std::size_t(-1)) {
+                            handler_(boost::asio::error::eof, bytes_recv_);
+                            delete this;
+                            return;
+                        }
+                        if (rcv_left_ == 0) {
+                            // 刚刚接收了尾部，重组新的Chunk
+                            if (bytes_left_ == 0) {
+                                handler_(ec, bytes_recv_);
+                                delete this;
+                                return;
+                            }
+                            recv_crlf(rcv_buf_, rcv_left_);
+                            socket_.async_receive(rcv_buf_.prepare(rcv_left_), flags_, 
+                                boost::bind(boost::ref(*this), _1, _2));
+                        } else {
+                            socket_.async_receive(buffers::sub_buffers(buffers_, bytes_recv_, bytes_left_), flags_, 
+                                boost::bind(boost::ref(*this), _1, _2));
+                        }
+                    } else {
+                        // 接收完头部，接收剩余数据
+                        bytes_recv_ += bytes_transferred;
+                        bytes_left_ -= bytes_transferred;
+                        rcv_left_ -= bytes_transferred;
+                        if (rcv_left_ == 0) {
+                            // 接收完数据，构建尾部
+                            recv_crlf(rcv_buf_, rcv_left_ = 1);
+                        }
+                        handler_(ec, bytes_recv_);
+                        delete this;
+                        return;
+                    }
+                }
+
+                PASS_DOWN_ASIO_HANDLER_FUNCTION(recv_handler, handler_)
+
+            private:
+                Source & socket_;
+                std::size_t & rcv_left_;
+                boost::asio::streambuf & rcv_buf_;
+                MutableBufferSequence buffers_;
+                boost::asio::socket_base::message_flags flags_;
+                ReadHandler handler_;
+                std::size_t bytes_recv_;
+                std::size_t bytes_left_;
+            };
 
             template <typename MutableBufferSequence, typename ReadHandler>
             void async_receive(
                 const MutableBufferSequence& buffers,
                 ReadHandler handler)
             {
-                if (rcv_buf_.size() > 0) {
-                    std::size_t length = copy(buffers);
-                    socket_.get_io_service().post(boost::asio::detail::bind_handler(
-                        handler, boost::system::error_code(), length));
-                } else {
-                    socket_.async_receive(buffers, handler);
-                }
+                async_receive(buffers, 0, handler);
             }
 
             template <typename MutableBufferSequence, typename ReadHandler>
@@ -167,24 +376,18 @@ namespace util
                 boost::asio::socket_base::message_flags flags, 
                 ReadHandler handler)
             {
-                if (rcv_buf_.size() > 0) {
-                    std::size_t length = copy(buffers);
-                    socket_.get_io_service().post(boost::asio::detail::bind_handler(
-                        handler, boost::system::error_code(), length));
-                } else {
-                    socket_.async_receive(buffers, flags, handler);
-                }
+                (new recv_handler<ConstBufferSequence, WriteHandler>(
+                    socket_, snd_left_, snd_buf_, buffers, flags, handler))->start();
             }
 
             template <typename MutableBufferSequence>
             std::size_t read_some(
                 const MutableBufferSequence& buffers)
             {
-                if (rcv_buf_.size() > 0) {
-                    return copy(buffers);
-                } else {
-                    return socket_.read_some(buffers);
-                }
+                boost::system::error_code ec;
+                std::size_t s = recv(buffers, 0, ec);
+                boost::asio::detail::throw_error(ec);
+                return s;
             }
 
             template <typename MutableBufferSequence>
@@ -192,51 +395,15 @@ namespace util
                 const MutableBufferSequence& buffers,
                 boost::system::error_code& ec)
             {
-                if (rcv_buf_.size() > 0) {
-                    ec = boost::system::error_code();
-                    return copy(buffers);
-                } else {
-                    return socket_.read_some(buffers, ec);
-                }
+                return recv(buffers, 0, ec);
             }
 
             template <typename MutableBufferSequence, typename ReadHandler>
-            void async_read_some(
+            void async_receive(
                 const MutableBufferSequence& buffers,
                 ReadHandler handler)
             {
-                if (rcv_buf_.size() > 0) {
-                    std::size_t length = copy(buffers);
-                    socket_.get_io_service().post(boost::asio::detail::bind_handler(
-                        handler, boost::system::error_code(), length));
-                } else {
-                    socket_.async_read_some(buffers, handler);
-                }
-            }
-
-            template <typename MutableBufferSequence>
-            std::size_t copy(
-                const MutableBufferSequence& buffers)
-            {
-                using namespace std; // For memcpy.
-                using namespace boost::asio;
-
-                std::size_t bytes_avail = rcv_buf_.size();
-                std::size_t bytes_copied = 0;
-
-                typename MutableBufferSequence::const_iterator iter = buffers.begin();
-                typename MutableBufferSequence::const_iterator end = buffers.end();
-                for (; iter != end && bytes_avail > 0; ++iter) {
-                    std::size_t max_length = buffer_size(*iter);
-                    std::size_t length = (max_length < bytes_avail)
-                        ? max_length : bytes_avail;
-                    memcpy(buffer_cast<void *>(*iter), buffer_cast<char const *>(rcv_buf_.data()) + bytes_copied, length);
-                    bytes_copied += length;
-                    bytes_avail -= length;
-                }
-
-                rcv_buf_.consume(bytes_copied);
-                return bytes_copied;
+                async_receive(buffers, 0, handler);
             }
 
             // 重写send，async_send，write_some，async_write_some
@@ -341,6 +508,8 @@ namespace util
             template <typename ConstBufferSequence, typename WriteHandler>
             struct send_handler
             {
+                typedef void result_type;
+
                 send_handler(
                     Socket & socket, 
                     std::size_t & snd_left, 
@@ -375,10 +544,10 @@ namespace util
                             snd_left_ = bytes_left_;
                             make_chunk_head(snd_buf_, snd_left_);
                             socket_.async_send(buffers::sub_buffers(buffers_, bytes_send_, bytes_left_), flags_, 
-                                 boost::bind(boost::ref(*this)));
+                                 boost::bind(boost::ref(*this), _1, _2));
                         } else {
                             socket_.async_send(buffers::sub_buffers(buffers_, bytes_send_, bytes_left_), flags_,
-                                 boost::bind(boost::ref(*this)));
+                                 boost::bind(boost::ref(*this), _1, _2));
                         }
                     }
                 }
@@ -405,10 +574,10 @@ namespace util
                             snd_left_ = bytes_left_;
                             make_chunk_head(snd_buf_, snd_left_);
                             socket_.async_send(snd_buf_.data(), flags_,
-                                boost::bind(boost::ref(*this)));
+                                boost::bind(boost::ref(*this), _1, _2));
                         } else {
                             socket_.async_send(buffers::sub_buffers(buffers_, bytes_send_, bytes_left_), flags_,
-                                boost::bind(boost::ref(*this)));
+                                boost::bind(boost::ref(*this), _1, _2));
                         }
                     } else {
                         // 发送完头部，发送剩余数据
@@ -423,35 +592,11 @@ namespace util
                         // 发送完数据，构建尾部
                         make_chunk_tail(snd_buf_);
                         socket_.async_send(snd_buf_.data(), flags_,
-                            boost::bind(boost::ref(*this)));
+                            boost::bind(boost::ref(*this), _1, _2));
                     }
                 }
 
-                inline friend void* asio_handler_allocate(
-                    std::size_t size,
-                    send_handler * this_handler)
-                {
-                    return boost_asio_handler_alloc_helpers::allocate(
-                        size, &this_handler->handler_);
-                }
-
-                inline friend void asio_handler_deallocate(
-                    void * pointer, 
-                    std::size_t size,
-                    send_handler * this_handler)
-                {
-                    boost_asio_handler_alloc_helpers::deallocate(
-                        pointer, size, &this_handler->handler_);
-                }
-
-                template <typename Function>
-                inline friend void asio_handler_invoke(
-                    const Function & function,
-                    send_handler * this_handler)
-                {
-                    boost_asio_handler_invoke_helpers::invoke(
-                        function, &this_handler->handler_);
-                }
+                PASS_DOWN_ASIO_HANDLER_FUNCTION(send_handler, handler_)
 
             private:
                 Socket & socket_;
