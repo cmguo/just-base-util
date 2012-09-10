585aa8ea62f32db278cbdd7c60ec1812572e0f80
isxxguo <isxxguo@pptv.com>
2012-09-10 07:10:12 +0000

    /base/branches/1.2.2 /include/util/serialization/Optional.h 增加make_optional方式，直接将系列化项作为可选项处理
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.2@4804 b3a1389e-2159-5c43-9038-463bf877d0d9

--- serialization/Optional.h
+++ serialization/Optional.h
@@ -18,6 +18,48 @@ namespace util
         {
         };
 
+        template<
+            class _T
+        >
+        struct optional_ref
+        {
+            optional_ref(
+                _T & t, 
+                size_t ver)
+                : t_(t)
+                , ver_(ver)
+            {
+            }
+
+            template <
+                typename Archive
+            >
+            void serialize( 
+                Archive & ar)
+            {
+                size_t ver = ar.version();
+                if (ver_ > 0)
+                    ar.version(ver_);
+                ar & t_;
+                if (ver_ > 0)
+                    ar.version(ver);
+            }
+
+        private:
+            _T & t_;
+            size_t ver_;
+        };
+
+        template<
+            class _T
+        >
+        optional_ref<_T> make_optional(
+            _T & t, 
+            size_t ver = 0)
+        {
+            return optional_ref<_T>(t, ver);
+        }
+
     }  // namespace serialize
 } // namespace util
 
