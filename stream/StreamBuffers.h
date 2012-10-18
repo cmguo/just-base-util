cf5b69542a3227bae5799ea5d33bcc845c92815c
isxxguo <isxxguo@pptv.com>
2012-10-18 02:33:05 +0000

    /base/branches/1.2.2 /include/util/stream/StreamBuffers.h 增加 push_back,reset,clear，operator=等接口
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.2@5268 b3a1389e-2159-5c43-9038-463bf877d0d9

--- stream/StreamBuffers.h
+++ stream/StreamBuffers.h
@@ -38,10 +38,7 @@ namespace util
                 typename BufferSequence::const_iterator end = buffers.end();
                 size_t count = std::distance(beg, end);
                 if (count > 0) {
-                    buf_ = new Buf;
-                    buf_->nref = 1;
-                    buf_->count = count;
-                    buf_->buffers = new Buffer[count];
+                    reserve(count);
                     std::copy(beg, end, buf_->buffers);
                 }
             }
@@ -50,7 +47,9 @@ namespace util
                 StreamBuffers const & r)
                 : buf_(r.buf_)
             {
-                ++buf_->nref;
+                if (buf_) {
+                    ++buf_->nref;
+                }
             }
 
             ~StreamBuffers()
@@ -64,18 +63,97 @@ namespace util
         public:
             const_iterator begin() const
             {
-                return buf_->buffers;
+                return buf_ ? buf_->buffers : NULL;
             }
 
             const_iterator end() const
             {
-                return buf_->buffers + buf_->count;
+                return buf_ ? buf_->buffers + buf_->count : NULL;
+            }
+
+            size_t size() const
+            {
+                return buf_ ? buf_->count : 0;
+            }
+
+            size_t capacity() const
+            {
+                return buf_ ? buf_->capacity : 0;
             }
 
         public:
             void reset()
             {
-                swap(StreamBuffers(), *this);
+                StreamBuffers tmp;
+                std::swap(tmp.buf_, buf_);
+            }
+
+            void reset(
+                StreamBuffers const & r)
+            {
+                StreamBuffers tmp(r);
+                std::swap(tmp.buf_, buf_);
+            }
+
+            StreamBuffers & operator=(
+                StreamBuffers const & r)
+            {
+                reset(r);
+                return *this;
+            }
+
+        public:
+            void reserve(
+                size_t count)
+            {
+                if (buf_) {
+                    if (buf_->nref > 1) {
+                        StreamBuffers tmp;
+                        std::swap(tmp.buf_, buf_);
+                        buf_ = new Buf;
+                        buf_->nref = 1;
+                        buf_->count = tmp.buf_->count;
+                        buf_->capacity = count;
+                        if (buf_->capacity < buf_->count)
+                            buf_->capacity = buf_->count;
+                        buf_->buffers = new Buffer[buf_->capacity];
+                        if (buf_->count)
+                            std::copy(tmp.begin(), tmp.end(), buf_->buffers);
+                    } else if (count > buf_->capacity) {
+                        Buffer * buffers = NULL;
+                        std::swap(buffers, buf_->buffers);
+                        buf_->capacity = count;
+                        buf_->buffers = new Buffer[buf_->capacity];
+                        if (buf_->count)
+                            std::copy(buffers, buffers + buf_->count, buf_->buffers);
+                        delete [] buffers;
+                    }
+                } else if (count > 0) {
+                    buf_ = new Buf;
+                    buf_->nref = 1;
+                    buf_->count = 0;
+                    buf_->capacity = count;
+                    buf_->buffers = new Buffer[buf_->capacity];
+                }
+            }
+
+            void push_back(
+                Buffer const & buffer)
+            {
+                reserve(size() + 1);
+                buf_->buffers[buf_->count] = buffer;
+                ++buf_->count;
+            }
+
+            void clear()
+            {
+                if (buf_) {
+                    if (buf_->nref > 1) {
+                        reset();
+                    } else {
+                        buf_->count = 0;
+                    }
+                }
             }
 
         private:
@@ -83,6 +161,7 @@ namespace util
             {
                 size_t nref;
                 size_t count;
+                size_t capacity;
                 Buffer * buffers;
             };
 
