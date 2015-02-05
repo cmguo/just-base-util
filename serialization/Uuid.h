eca59d10b0095996d43362e127407009d3dd506e
isxxguo <isxxguo@pptv.com>
2015-02-05 08:29:27 +0800

    修正 Uuid 序列化函数的名字空间
    Change-Id: Ic559831a1e925670379395275110e2e8fb77c607

--- serialization/Uuid.h
+++ serialization/Uuid.h
@@ -9,19 +9,19 @@
 
 #include <framework/string/Uuid.h>
 
-namespace util
+namespace frameowrk
 {
-    namespace serialization
+    namespace string
     {
 
         template <typename Archive>
-        void serialize(Archive & ar, framework::string::Uuid & t)
+        void serialize(Archive & ar, Uuid & t)
         {
             util::serialization::split_free(ar, t);
         }
 
         template <typename Archive>
-        void load(Archive & ar, framework::string::Uuid & t)
+        void load(Archive & ar, Uuid & t)
         {
             framework::string::UUID uuid;
             ar & SERIALIZATION_NVP_1(uuid, Data1)
@@ -34,7 +34,7 @@ namespace util
         };
 
         template <typename Archive>
-        void save(Archive & ar, framework::string::Uuid const & t)
+        void save(Archive & ar, Uuid const & t)
         {
             framework::string::UUID const & uuid = t.data();
             ar & SERIALIZATION_NVP_1(uuid, Data1)
@@ -43,6 +43,7 @@ namespace util
                 & util::serialization::make_nvp("Data4", 
                     framework::container::make_array(uuid.Data4, sizeof(uuid.Data4)));
         }
+
     }
 }
 
