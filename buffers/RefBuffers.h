8aab5b543d2e42fcb93cd51efe2e0feb5eb65ddd
isxxguo <isxxguo@pptv.com>
2015-03-09 13:28:25 +0800

    增加  RefBuffers

---
+++ buffers/RefBuffers.h
@@ -0,0 +1,53 @@
+// RefBuffers.h
+
+#ifndef _UTIL_BUFFERS_REF_BUFFERS_H_
+#define _UTIL_BUFFERS_REF_BUFFERS_H_
+
+namespace util
+{
+    namespace buffers
+    {
+
+        template <
+            typename BufferSequence
+        >
+        class RefBuffers
+        {
+        public:
+            typedef typename BufferSequence::const_iterator const_iterator;
+
+        public:
+            RefBuffers(
+                BufferSequence const & buffers)
+                : buffers_(buffers)
+            {
+            }
+
+            const_iterator begin() const
+            {
+                return buffers_.begin();
+            }
+
+            const_iterator end() const
+            {
+                return buffers_.end();
+            }
+
+        private:
+            BufferSequence const & buffers_;
+        };
+
+        template <
+            typename BufferSequence
+        >
+        RefBuffers<BufferSequence> const ref_buffers(
+            BufferSequence const & buffers)
+        {
+            return RefBuffers<BufferSequence>(buffers);
+        }
+
+    } // namespace buffers
+} // namespace util
+
+#endif // _UTIL_BUFFERS_REF_BUFFERS_H_
+
