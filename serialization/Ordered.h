4c36be8f7d22c727b539f5e81df0608646a924ea
isxxguo <isxxguo@pptv.com>
2016-09-01 17:02:40 +0800

    serialization: add serialize of Ordered
    Change-Id: I81bf779f079bdeab8e22b1b2674f9f4e96c5c01e

---
+++ serialization/Ordered.h
@@ -0,0 +1,43 @@
+// Ordered.h
+
+#ifndef _UTIL_SERIALIZATION_ORDERED_H_
+#define _UTIL_SERIALIZATION_ORDERED_H_
+
+#include "util/serialization/Collection.h"
+#include "util/serialization/SplitMember.h"
+
+#include <list>
+
+namespace util
+{
+    namespace serialization
+    {
+
+        template<
+            class Archive, 
+            class _Ty, 
+            class _Key, 
+            typename _Ls, 
+            typename _Cat
+        >
+        struct is_collection<Archive, framework::container::Ordered<_Ty, _Key, _Ls, _Cat> >
+            : boost::true_type
+        {
+        };
+
+    }  // namespace serialization
+} // namespace util
+
+namespace framework { namespace container {
+
+    template<class Archive, class _Ty, class _Key, typename _Ls, typename _Cat>
+    inline void serialize(
+        Archive & ar,
+        Ordered<_Ty, _Key, _Ls, _Cat> & t)
+    {
+        util::serialization::serialize_collection(ar, t);
+    }
+
+} }
+
+#endif // _UTIL_SERIALIZATION_ORDERED_H_
